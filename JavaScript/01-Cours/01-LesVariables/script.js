1a09712ad4aa1af2f0c622dd8d715825d90d6e1c
Anthony Di Persio <anthony@utopios.net>
2022-05-23 09:57:30 +0200

    Ajout starter JS 1

---
+++ JavaScript/01-Cours/01-LesVariables/script.js
@@ -0,0 +1,17 @@
+// Commentaire sur une seule ligne
+
+/*
+    Commentaire Multilignes
+*/
+
+/**
+ *  Commentaire multilignes
+ */
+
+
+/**
+ *  LES VARIABLES
+ */
+
+// Déclaration d'une variable ne JS (ES5)
+var prenom;
