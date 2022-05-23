0fdeffe3100332bfe3ff4b7397e7aa4f49e2a060
Anthony Di Persio <anthony@utopios.net>
2022-05-23 10:41:19 +0200

    Cours JS Part 1

--- JavaScript/01-Cours/01-LesVariables/script.js
+++ JavaScript/01-Cours/01-LesVariables/script.js
@@ -8,10 +8,48 @@
  *  Commentaire multilignes
  */
 
-
 /**
  *  LES VARIABLES
  */
 
 // Déclaration d'une variable ne JS (ES5)
 var prenom;
+
+// Affichage de la valeur de prenom dans la console
+console.log(prenom);
+
+// Affichage du type de la variable dans la console
+console.log(typeof(prenom));
+
+// Affectaction de valeur à la variable prenom
+prenom = "Anthony";
+
+// Affichage de la valeur de prenom dans la console
+console.log(prenom);
+
+// Affichage du type de la variable dans la console
+console.log(typeof(prenom));
+
+// console.clear();
+
+// Déclaration et Affectaction de valeur à la variable nom
+var nom = "Di Persio";
+
+// Affichage de la valeur de nom dans la console
+console.log(nom);
+
+// Affichage du type de la variable dans la console
+console.log(typeof(nom));
+
+// Déclaration d'une varaible Age et affectaction de valeur
+var age = 20;
+
+// Affichage de la valeur de nom dans la console
+console.log(age);
+
+// Affichage du type de la variable dans la console
+console.log(typeof(age));
+
+// Concaténation des deux variables
+console.log("Je m'appelle " + nom + " " + prenom + " et j'ai "+ age + " ans.");
+
