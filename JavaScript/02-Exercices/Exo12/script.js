9c489f0af8148d6458b1a611a01a27fdc98df89d
Anthony Di Persio <anthony@utopios.net>
2022-05-31 09:37:04 +0200

    correction exo 12

---
+++ JavaScript/02-Exercices/Exo12/script.js
@@ -0,0 +1,29 @@
+/**
+ * EXERCICE 12 - LES STRUCTURES CONDITIONNELLES - If...Else... Le nombre est-il divisible par...?
+ */
+
+
+// Création de la constante result permettant de recupérer l'élément HTML id="result"
+const result = document.querySelector('#result');
+
+// Affichage du titre dans le HTML
+var affichage = `<br/><h3>Le nombre est-il divisible par...?</h3>`;
+var nb1 = 0,nb2 =0;
+console.log(typeof(nb1));
+// Récupération des saisies utilisateur pour le coté du carré
+nb1 = Number(prompt("Veuillez un chiffre / nombre : "));
+nb2 = Number(prompt("Veuillez un chiffre / nombre diviseur : "));
+
+console.log(typeof(nb1));
+// Mise en place d'une structure conditionnelle pour vérifier si la nb1 est divisible par nb2
+if (nb1 % nb2 == 0)    
+        affichage += `le nombre ${nb1} est divisible par ${nb2}`;    
+    else
+        affichage += `le nombre ${nb1} n'est pas divisible par ${nb2}`;
+
+
+
+// Affichage du contenu de la variable affichage dans l'element HTML #result
+result.innerHTML = affichage;
+
+
