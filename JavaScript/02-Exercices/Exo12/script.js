234306e19bdfb9772f57517f179e2011ecc98272
Anthony Di Persio <anthony@utopios.net>
2022-05-31 10:15:08 +0200

    correction exo 13

--- JavaScript/02-Exercices/Exo12/script.js
+++ JavaScript/02-Exercices/Exo12/script.js
@@ -8,19 +8,18 @@ const result = document.querySelector('#result');
 
 // Affichage du titre dans le HTML
 var affichage = `<br/><h3>Le nombre est-il divisible par...?</h3>`;
-var nb1 = 0,nb2 =0;
-console.log(typeof(nb1));
+var nb1 = 0, nb2 = 0;
+console.log(typeof (nb1));
 // Récupération des saisies utilisateur pour le coté du carré
 nb1 = Number(prompt("Veuillez un chiffre / nombre : "));
 nb2 = Number(prompt("Veuillez un chiffre / nombre diviseur : "));
 
-console.log(typeof(nb1));
-// Mise en place d'une structure conditionnelle pour vérifier si la nb1 est divisible par nb2
-if (nb1 % nb2 == 0)    
-        affichage += `le nombre ${nb1} est divisible par ${nb2}`;    
-    else
-        affichage += `le nombre ${nb1} n'est pas divisible par ${nb2}`;
-
+console.log(typeof (nb1));
+// Mise en place d'une structure conditionnelle pour vérifier si la nb1 est divisible par nb2  
+if (nb1 % nb2 == 0)
+    affichage += `le nombre <b>${nb1}</b> est divisible par <b>${nb2}</b>`;
+else
+    affichage += `le nombre <b>${nb1}</b> n'est pas divisible par <b>${nb2}</b>`;
 
 
 // Affichage du contenu de la variable affichage dans l'element HTML #result
