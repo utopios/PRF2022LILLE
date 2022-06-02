cecdc649ceae0a39af967eda73eb2e1831287497
Anthony Di Persio <anthony@utopios.net>
2022-06-02 11:46:19 +0200

    Correction exo 25 JS

---
+++ JavaScript/02-Exercices/Exo25/script.js
@@ -0,0 +1,63 @@
+/**
+ * EXERCICE 25 - LES STRUCTURES ITERATIVES - FOR - Chaines Entiers
+ */
+
+// Création de la constante result permettant de recupérer l'élément HTML id="result"
+const result = document.querySelector('#result');
+
+// Déclaration des variable
+let nombre = 0,
+    Affichage = "",
+    debut,
+    fin,
+    temps="";
+
+
+// Déclaration des variables
+Affichage += `<h2>Chaîne d'entiers</h2>`;
+
+// Récupératuion du nombre de l'utilisateur
+nombre = Number(prompt("Veuillez saisir un nombre entier"));
+
+// Affichage de la saisie utilisateur
+Affichage += nombre >= 0 && nombre < 10 ? `<p>Vous avez saisi le chiffre <b>${nombre}</b></p>` : `<p>Vous avez saisi le nombre <b>${nombre}</b></p>`;
+//console.log(isNaN(nombre));
+// Verifier si l'utilisateur a saisi un nombre
+if (!isNaN(nombre)) {
+    Affichage +=`<p>Voici la liste d'entiers chaîné dont la somme est égale à <b>${nombre}</b></p><ul>`;
+    // Métrique du code
+    //debut = Date.now();
+    console.time(temps);
+    console.log(debut);
+    for (let i = 1; i <= nombre / 2 + 1; i++) {
+        let chaine = `${nombre} = ${i}`;
+        let somme = i;
+        for (let j = i + 1; j <= nombre / 2 + 1; j++) {
+            somme += j;
+            chaine += ` + ${j}`;
+            // console.log("Somme = " +somme);
+            // console.log("chaine = " +chaine);
+
+            if (somme === nombre) {
+                Affichage += `<li>${chaine}</li>`;
+                //break;
+            }
+            else if (somme > nombre) {
+                //break;
+            }
+        }
+    }
+    // fin = Date.now();
+    console.timeEnd(temps);
+  
+    Affichage +="</ul>";
+}
+
+
+
+
+// Affichage += `<p>Avec un taux d'accroissement de la population de <b>${taux}%</b>,en <b>${date}</b>, il y aura <b>${Math.round(nbHabitant)} habitants</b> dans la ville de <b>Tourcoing</b>. Il aura fallu <b>${nbAnnee} année(s)</b>.</p>`
+
+// Affichage du contenu de la variable indemnité dans l'element HTML #result
+result.innerHTML = Affichage;
+
