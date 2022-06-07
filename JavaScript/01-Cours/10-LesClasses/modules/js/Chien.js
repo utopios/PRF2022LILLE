468abcea406dbd5364219939f5b8d1d619f64d1a
Anthony Di Persio <anthony@utopios.net>
2022-06-07 12:42:21 +0200

    Add Poo / Heritage cours

---
+++ JavaScript/01-Cours/10-LesClasses/modules/js/Chien.js
@@ -0,0 +1,19 @@
+import Mammifere from './Mammifere.js';
+
+export default class Chien extends Mammifere {
+
+    Alimentation() {
+        console.log("Je me nourris de croquettes... et d'os à moelle!");
+    }
+    Respiration() {
+        console.log("Inspiration, Expiration... J'ai des poumons!");
+    }
+    Aboyer() {
+        if(this.heartRate){
+            console.log("Wouaf!!!");
+        }
+        else{
+            console.log("Je ne peux pas... je ne suis plus!");
+        }
+    }
+}
--- JavaScript/01-Cours/10-LesClasses/script.js
+++
@@ -1,4 +0,0 @@
-/**
- * Les Classes en JS (POO)
- */
-
