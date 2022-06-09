fbf754edd97cfd5f1feab887830b7e05b621257f
Anthony Di Persio <anthony@utopios.net>
2022-06-09 09:46:54 +0200

    fin correction TP Contact

--- JavaScript/02-Exercices/PooJS/03-TpListeContact/module/main.js
+++ JavaScript/02-Exercices/PooJS/03-TpListeContact/module/main.js
@@ -2,6 +2,7 @@ import Contact from './js/Contact.js';
 
 //Création des constantes permettant la récupération d'element HTML
 const tableau = document.querySelector('#tableau');
+const ValiderBtn = document.querySelector('#validerBtn');
 
 // Declaration des variables globale
 let contacts = [];
@@ -35,23 +36,51 @@ function createFakeContact() {
         }
     ];
     for (let contact of annuaire) {
-        let cTmp = new Contact(contact.titre, contact.nom, contact.prenom,new Date(contact.dateNaissance).toLocaleDateString(),contact.telephone,contact.email);
-        contacts.push(cTmp);
+        // let cTmp = new Contact(contact.titre, contact.nom, contact.prenom,new Date(contact.dateNaissance).toLocaleDateString(),contact.telephone,contact.email);
+        // contacts.push(cTmp);
+
+        contacts.push(new Contact(contact.titre, contact.nom, contact.prenom, new Date(contact.dateNaissance).toLocaleDateString(), contact.telephone, contact.email));
     }
     insertList();
 }
 
-function insertList(){
-    for(let contact of contacts){
-        tableau.innerHTML +=`<tr>
+function insertList() {
+    tableau.innerHTML = "";
+    for (let contact of contacts) {
+        tableau.innerHTML += `<tr>
                                 <td>${contact.titre}</td>
                                 <td>${contact.nom}</td>
                                 <td>${contact.prenom}</td>
                                 <td>${contact.dateNaissance}</td>
                                 <td>${contact.telephone}</td>
-                                <td>${contact.email}</td>        
+                                <td>${contact.adresse}</td>        
                             </tr>`;
     }
+}
+
+function resetForm() {
+    document.querySelector('#nom').value = "";
+    document.querySelector('#prenom').value = "";
+    document.querySelector('#dateNaissance').value = "";
+    document.querySelector('#telephone').value = "";
+    document.querySelector('#email').value = "";
+}
+
+ValiderBtn.onclick = () => {
+
+
+    // Récupération des saisies de l'utilisateur
+    const nom = document.querySelector('#nom').value;
+    const prenom = document.querySelector('#prenom').value;
+    const dateNaissance = document.querySelector('#dateNaissance').value;
+    const telephone = document.querySelector('#telephone').value;
+    const email = document.querySelector('#email').value;
+    // Titre
+    let titre = document.getElementById("Mr").checked ? "Mr" : "Mme";
+
+    contacts.push(new Contact(titre, nom, prenom, new Date(dateNaissance).toLocaleDateString(), telephone, email));
+    insertList();
+    resetForm();
 }
 
 window.onload = createFakeContact();
