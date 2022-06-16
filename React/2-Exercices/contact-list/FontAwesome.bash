766f5bec4b30b8a6cfe9875a5560da5f88085db7
Anthony Di Persio <anthony@utopios.net>
2022-06-16 13:49:00 +0200

    add fontAwesome

---
+++ React/2-Exercices/contact-list/FontAwesome.bash
@@ -0,0 +1,24 @@
+####
+## Install Fontawesome
+####
+
+# Installer tous en une seule fois
+npm i -S @fortawesome/fontawesome-svg-core @fortawesome/react-fontawesome @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons @fortawesome/free-brands-svg-icons
+
+# Exécutez la commande suivante pour installer les paquets de base :
+npm i -S @fortawesome/fontawesome-svg-core @fortawesome/react-fontawesome
+# regular icons
+npm i -S @fortawesome/free-regular-svg-icons
+# solid icons
+npm i -S @fortawesome/free-solid-svg-icons
+# brand icons
+npm i -S @fortawesome/free-brands-svg-icons
+
+## Import Fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+# Import solid 
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+# Import Regular
+import { faHome } from "@fortawesome/free-regular-svg-icons";
+# Import brand
+import { faHome } from "@fortawesome/free-brand-svg-icons";
--- React/2-Exercices/contact-list/src/App.js
+++ React/2-Exercices/contact-list/src/App.js
@@ -1,21 +1,30 @@
-import {React , useState} from 'react';
+import { React, useEffect, useState } from 'react';
 import Contact from './models/Contact';
-import {ListPersons} from './data/ListPerson';
+import { ListPersons } from './data/ListPerson';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBarComponent/NavBar';
 
 function App() {
+  // Récupération des données stockée dans le local storage
+  const savedList = JSON.parse(localStorage.getItem('List'));
 
   // Création d'une liste de contact dpuis notre ListPersons
-  const contactList = ListPersons.map((contact)=> new Contact(contact.nom, contact.prenom, contact.email
-    , contact.telephone));
+  const contactList = savedList ?
+    savedList.map((contact) => new Contact(contact.nom, contact.prenom, contact.email
+      , contact.telephone))
+    :
+    null;
 
-  const [ListContact, updateListContact] = useState(contactList? contactList : []);
+  const [ListContact, updateListContact] = useState(contactList ? contactList : []);
+
+  useEffect(()=> {
+    localStorage.setItem('List', JSON.stringify(ListContact))
+  })
 
   return (
     <div className="App">
-      <NavBar ListContact={ListContact} updateListContact={updateListContact}/>
+      <NavBar ListContact={ListContact} updateListContact={updateListContact} />
     </div>
   );
 }
