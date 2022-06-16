dc8375ef47e85793672c1f53606daedbffc6a31a
Anthony Di Persio <anthony@utopios.net>
2022-06-16 14:35:27 +0200

    Add FontAwesome

--- React/2-Exercices/contact-list/src/components/NavBarComponent/NavBar.jsx
+++ React/2-Exercices/contact-list/src/components/NavBarComponent/NavBar.jsx
@@ -10,7 +10,8 @@ import AddContact from '../../views/AddContactView/AddContact';
 import ContactListView from '../../views/ContactListView/ContactListView';
 import Home from '../../views/HomeView/Home';
 import ErrorComponent from '../ErrorComponent/ErrorComponent';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faAddressCard, faFolderPlus, faHomeAlt } from "@fortawesome/free-solid-svg-icons";
 import './NavBar.css';
 
 const NavBar = ({ListContact, updateListContact}) => {
@@ -18,9 +19,19 @@ const NavBar = ({ListContact, updateListContact}) => {
         <div>
             <BrowserRouter>
                 <div className='navBar'>
-                    <Link to='/' className='btn btn-secondary navBarBtn'>Home</Link>
+                    {/* <Link to='/' className='btn btn-secondary navBarBtn'>Home</Link>
                     <Link to='/list' className='btn btn-secondary navBarBtn'>List</Link>
-                    <Link to='/add' className='btn btn-secondary navBarBtn'>Add</Link>
+                    <Link to='/add' className='btn btn-secondary navBarBtn'>Add</Link> */}
+
+                    <Link to="/">
+                        <FontAwesomeIcon className="iconMenu" icon={faHome}/>
+                    </Link>
+                    <Link to="/list">
+                        <FontAwesomeIcon className="iconMenu" icon={faAddressCard}/>
+                    </Link>
+                    <Link to="/add">
+                        <FontAwesomeIcon className="iconMenu" icon={faFolderPlus}/>
+                    </Link>
                     <hr/>
                 </div>
                 <Routes>
--- React/2-Exercices/contact-list/src/views/HomeView/Home.jsx
+++ React/2-Exercices/contact-list/src/views/HomeView/Home.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+    faHome,
+    faAddressCard,
+    faBatteryEmpty,
+    faBatteryQuarter,
+    faBatteryHalf,
+    faBatteryThreeQuarters,
+    faBatteryFull
+} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import './Home.css';
+
 
 const Home = () => {
     return (
         <div>
             <h1>TP Annuaire Contact - Bootstrap - Font Awesome</h1>
             <img src="./img/logo192.png" alt="React-Logo" />
+            <div>
+                <div>
+                    <FontAwesomeIcon icon={faBatteryEmpty} style={{ padding:'10px' , color: 'red', fontSize: '45px' }} />
+                    <FontAwesomeIcon icon={faBatteryQuarter} style={{ padding:'10px' , color: 'rgb(40, 44, 52)', fontSize: '45px' }} />
+                    <FontAwesomeIcon icon={faBatteryHalf} style={{ padding:'10px' , color: 'rgb(40, 44, 52)', fontSize: '45px' }} />
+                    <FontAwesomeIcon icon={faBatteryThreeQuarters} style={{ padding:'10px' , color: 'rgb(40, 44, 52)', fontSize: '45px' }} />
+                    <FontAwesomeIcon icon={faBatteryFull} style={{ padding:'10px' , color: 'green', fontSize: '45px' }} />
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faAddressCard} style={{ padding:'10px'}}/>
+                    <FontAwesomeIcon icon={faEnvelope} style={{ padding:'10px'}}/>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faHome} style={{ padding:'10px'}} />
+                    <FontAwesomeIcon icon={faHome} transform="down-4 grow-2.5" style={{ padding:'10px'}} />
+                    <FontAwesomeIcon icon={faHome} style={{ padding:'10px' ,  color: 'red', fontSize: '25px' }} />
+                </div>
+            </div>
+
         </div>
     );
 };
