0c8e71caad3173aeeff4bec74f944587c02a8f6a
PetitVulcan <dipersio.a@gmail.com>
2022-07-06 13:59:59 +0200

    Méthode TryParse

---
+++ FrameworkNET/FormationDotNET/FormationCSharp/Cours/TryParse/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// string chaine = "20";
+string chaine = "vingt";
+int age;
+bool result = int.TryParse(chaine, out age);
+
+if (int.TryParse(chaine, out age))
+    Console.WriteLine($"La convertion est possible, age vaut {age}");
+else
+    Console.WriteLine($"La convertion est impossible");
+
+
+Console.WriteLine(result);
+Console.WriteLine(age);
+
+
+Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
+Console.Read();
