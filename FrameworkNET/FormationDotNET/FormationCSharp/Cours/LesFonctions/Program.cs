1d52228259b965f971c9cc30c08902ca8c4ee3a2
Anthony Di Persio <anthony@utopios.net>
2022-07-08 14:23:19 +0200

    Add Cours Function

---
+++ FrameworkNET/FormationDotNET/FormationCSharp/Cours/LesFonctions/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercice33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = Console.ReadLine();
+            string prenom = Console.ReadLine();
+
+            //Console.WriteLine( nom + prenom );
+            Bonjour();
+        }
+        // Déclaration d'une fonction sans params
+        static void Bonjour()
+        {
+            Console.WriteLine("Bonjour!");
+        }
+
+    }
+}
+
+
+
+
---
+++ FrameworkNET/FormationDotNET/LesFonction5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LesFonction5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Bonjour();
+            Bonjour("Di Persio", "Anthony");
+            Console.WriteLine("Le résultat est : "+Additionner(3,5));
+            Display("C'est génial les fonctions!");
+            Console.Write("Veuillez saisir votre nom : ");
+            string nom = Console.ReadLine();
+            Console.Write("Veuillez saisir votre prénom : ");
+            string prenom = Console.ReadLine();
+            Console.Write("Veuillez saisir votre age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Display(nom, prenom, age);
+
+            Console.Read();
+        }
+
+        // Déclaration d'une fonction sans params
+        public static void Bonjour()
+        {
+            Console.WriteLine("Bonjour!");
+        }
+        public static void Bonjour(string nom,string prenom)
+        {
+            Console.WriteLine($"Bonjour {nom} {prenom}!");
+        }
+        public static int Additionner(int nb1,int nb2)
+        {
+            int resultat = nb1 + nb2;
+            return resultat;
+        }
+
+        public static void Display(string chaine)
+        {
+            Console.WriteLine(chaine);
+        }
+
+        public static void Display(string nom, string prenom,int age)
+        {
+            Console.WriteLine($"Bonjour {nom} {prenom}, vous avez {age} ans.");
+        }
+    }
+}
