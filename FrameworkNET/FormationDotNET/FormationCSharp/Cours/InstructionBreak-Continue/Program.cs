dc50265aeb0e78a1686ce249bd306185b3d5b1ad
Anthony Di Persio <anthony@utopios.net>
2022-07-08 14:58:56 +0200

    Add Break Continu Cours

---
+++ FrameworkNET/FormationDotNET/FormationCSharp/Cours/InstructionBreak-Continue/Program.cs
@@ -0,0 +1,25 @@
+﻿int compteur = 0;
+
+
+for (int i = 0; i < 100; i++)
+{
+    compteur++;
+    if (compteur ==50)
+    {        
+        continue;
+    }
+    Console.WriteLine(compteur);
+    if (compteur == 55)
+    {
+        break;
+    }
+
+}
+
+
+
+
+
+
+Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
+Console.Read();
