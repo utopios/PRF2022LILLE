471e1abec6ed5160a67c6673395d194ede742866
Anthony Di Persio <anthony@utopios.net>
2022-07-11 12:35:35 +0200

    add Object cours Part 1

---
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/IntroductionPOO/Classes/Voiture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionPOO.Classes
+{
+    internal class Voiture
+    {
+        #region Attibuts (Privé)
+        private string modele;
+        private string couleur;
+        private int reservoir;
+        private int autonomie;
+        #endregion
+
+        #region Constructeurs
+
+        public Voiture()
+        {
+
+        }
+
+        public Voiture(string modele, string couleur, int reservoir, int autonomie)
+        {
+            Modele = modele;
+            Couleur = couleur;
+            Reservoir = reservoir;
+            Autonomie = autonomie;
+        }
+
+        #endregion
+
+        #region Proprietes (Publiques)
+        public string Modele { get => modele; set => modele = value; }
+        public string Couleur { get => couleur; set => couleur = value; }
+        public int Reservoir { get => reservoir; set => reservoir = value; }
+        public int Autonomie { get => autonomie; set => autonomie = value; }
+        #endregion
+
+        #region Méthodes (Fonction appartenant à une classe)
+        public override string ToString()
+        {
+            return $"La voiture est une {Modele} de couleur {Couleur}.\n" +
+                     $"Elle a un réservoir de {Reservoir} litres pour une autonomie de {Autonomie} km.";
+        }
+        #endregion
+    }
+}
---
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/IntroductionPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿using IntroductionPOO.Classes;
+
+// Création d'une instance de la classe Voiture
+Voiture voitureDeNicolas = new Voiture();
+// Ajout des propriétés de notre objet voiture
+voitureDeNicolas.Modele = "Clio";
+voitureDeNicolas.Couleur = "Blanche";
+voitureDeNicolas.Reservoir = 45;
+voitureDeNicolas.Autonomie = 850;
+// Affchage de l'objet voitureDeNicolas dans la console
+Console.WriteLine(voitureDeNicolas);
+// Console.WriteLine(voitureDeNicolas.ToString());
+
+
+Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
+Console.Read();
