75348b80894efccb64dd1b447c665aa2ab2f9ee6
Anthony Di Persio <anthony@utopios.net>
2022-07-11 15:30:59 +0200

    Cours Part 2 + Sujet Tp POO Classe Chaise

--- FrameworkNET/FormationDotNET/FormationPOO/Cours/IntroductionPOO/Classes/Voiture.cs
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/IntroductionPOO/Classes/Voiture.cs
@@ -13,16 +13,19 @@ namespace IntroductionPOO.Classes
         private string couleur;
         private int reservoir;
         private int autonomie;
+        private bool demaree;
+        private bool roule;
         #endregion
 
         #region Constructeurs
 
         public Voiture()
         {
-
+            Demaree = false;
+            Roule = false;
         }
 
-        public Voiture(string modele, string couleur, int reservoir, int autonomie)
+        public Voiture(string modele, string couleur, int reservoir, int autonomie) : this()
         {
             Modele = modele;
             Couleur = couleur;
@@ -33,13 +36,107 @@ namespace IntroductionPOO.Classes
         #endregion
 
         #region Proprietes (Publiques)
-        public string Modele { get => modele; set => modele = value; }
+        public string Modele 
+        { 
+            get => modele;
+            set 
+            {
+                if (value.Length >=2)
+                {
+                    modele = value;
+                }
+                else
+                {
+                    Console.WriteLine("Erreur nom modele");
+                }
+            }  
+        }
         public string Couleur { get => couleur; set => couleur = value; }
         public int Reservoir { get => reservoir; set => reservoir = value; }
         public int Autonomie { get => autonomie; set => autonomie = value; }
+        public bool Demaree { get => demaree; set => demaree = value; }
+        public bool Roule { get => roule; set => roule = value; }
         #endregion
 
         #region Méthodes (Fonction appartenant à une classe)
+        public string Klaxonner()
+        {
+            return "Pouêt...Pouêt!!!";
+        }
+        public string Demarrer()
+        {
+            string response = "";
+            if (!Demaree)
+            {
+                Demaree = true;
+                response = "La voiture est démarée...,le moteur tourne!";
+            }
+            else
+                response = "Le moteur tourne déjà!";
+
+            return response;
+        }
+
+        public string Arreter()
+        {
+            string response = "";
+            if (Demaree)
+            {
+                if (!Roule)
+                {
+                    Demaree = false;
+                    response = "Le moteur est arrété";
+                }
+                else
+                    response = "Impossible d'arrété le moteur , la voiture roule!";
+            }
+            else
+                response = "Impossible d'arreter le moteur car il n'est pas démarré!";
+
+            return response;
+        }
+
+        public string Rouler()
+        {
+            string response = "";
+            if (Demaree)
+            {
+                if (Reservoir >= 10)
+                {
+                    if (!Roule)
+                    {
+                        roule = true;
+                        Reservoir -= 10;
+                        response = $"La voiture roule...\n\t Il vous reste {Reservoir} L de carburant";
+                    }
+                    else
+                        response = "Vous êtes déjà en train de rouler!";
+                }
+                else
+                    response = "Low Fuel ! Veuillez mettre du carburant!";
+            }
+            else
+                response = "Veuillez démarrer le véhicule avant de rouler!";
+
+            return response;
+        }
+
+        public string Stopper()
+        {
+            string response = "";
+
+            if (roule)
+            {
+                roule = false;                
+                response = $"La voiture est arrétée...\n\t Il vous reste {Reservoir} L de carburant";
+            }
+            else
+                response = "Vous n'êtes pas en train de rouler!";
+
+
+            return response;
+        }
+
         public override string ToString()
         {
             return $"La voiture est une {Modele} de couleur {Couleur}.\n" +
--- FrameworkNET/FormationDotNET/FormationPOO/Cours/IntroductionPOO/Program.cs
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/IntroductionPOO/Program.cs
@@ -7,10 +7,39 @@ voitureDeNicolas.Modele = "Clio";
 voitureDeNicolas.Couleur = "Blanche";
 voitureDeNicolas.Reservoir = 45;
 voitureDeNicolas.Autonomie = 850;
+
+Console.WriteLine("\nVoiture de Nicolas");
 // Affchage de l'objet voitureDeNicolas dans la console
 Console.WriteLine(voitureDeNicolas);
+Console.WriteLine(voitureDeNicolas.Demarrer());
+Console.WriteLine(voitureDeNicolas.Klaxonner());
+Console.WriteLine(voitureDeNicolas.Rouler());
+Console.WriteLine(voitureDeNicolas.Stopper());
+Console.WriteLine(voitureDeNicolas.Arreter());
 // Console.WriteLine(voitureDeNicolas.ToString());
 
+
+
+Console.WriteLine("\nVoiture de Julie");
+
+// Création d'une instance de Voiture par le contructeur à 4 params
+Voiture voitureDeJulie = new ("208","Rouge", 40, 800);
+Console.WriteLine(voitureDeJulie);
+
+Console.WriteLine(voitureDeJulie.Arreter());
+Console.WriteLine(voitureDeJulie.Rouler());
+Console.WriteLine(voitureDeJulie.Demarrer());
+Console.WriteLine(voitureDeJulie.Demarrer());
+Console.WriteLine(voitureDeJulie.Rouler());
+Console.WriteLine(voitureDeJulie.Stopper());
+Console.WriteLine(voitureDeJulie.Klaxonner());
+Console.WriteLine(voitureDeJulie.Arreter());
+Console.WriteLine(voitureDeJulie.Arreter());
+Console.WriteLine(voitureDeJulie.Rouler());
+Console.WriteLine(voitureDeJulie.Stopper());
+
+MyFunctions.Display(voitureDeJulie.ToString());
+Console.WriteLine(MyFunctions.compteur); 
 
 Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
 Console.Read();
