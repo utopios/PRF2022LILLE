4b295ea77e60464f22b30a8046ba2be068915a63
Anthony Di Persio <anthony@utopios.net>
2022-07-13 12:12:47 +0200

    Add TpSalarieHeritage Subject

---
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/Heritage/Classes/SuperChien.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage.Classes
+{
+    internal class SuperChien /*:Chien*/
+    {
+    }
+}
