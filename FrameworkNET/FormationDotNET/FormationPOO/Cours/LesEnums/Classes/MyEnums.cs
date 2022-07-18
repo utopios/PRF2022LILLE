6d2b78ad064d378c7847e630c7e8fdbaabbc71a8
Anthony Di Persio <anthony@utopios.net>
2022-07-18 11:48:30 +0200

    Add Cours Les Enums

---
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/LesEnums/Classes/MyEnums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesEnums.Classes
+{
+    internal class MyEnums
+    {
+        public enum Saison
+        {
+            Printemps, //0
+            Eté, //1
+            Automne, //2
+            Hiver //3
+        }
+    }
+}
---
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/LesEnums/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using static LesEnums.Classes.MyEnums;
+
+Saison a = Saison.Hiver;
+Console.WriteLine($"La valeur numérique de {a} est {(int)a}");
+
+var b = (Saison)1;
+Console.WriteLine(b);
+
+var c = (Saison)0;
+Console.WriteLine(c);
+
+
+Console.WriteLine("Appuyez sur ENTER pour fermer la console...");
+Console.Read();
