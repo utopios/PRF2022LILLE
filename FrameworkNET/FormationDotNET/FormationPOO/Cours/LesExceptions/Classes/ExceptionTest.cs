152198c1279c0a070d0095073a8df9eaec0d3139
Anthony Di Persio <anthony@utopios.net>
2022-07-19 10:37:50 +0200

    Add Les Exceptions Cours

---
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/LesExceptions/Classes/ExceptionTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesExceptions.Classes
+{
+    internal class ExceptionTest
+    {
+        public static double SafeDivision(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Attention, vous tentez une division par zéro.");
+            return a / b;
+        }
+    }
+}
---
+++ FrameworkNET/FormationDotNET/FormationPOO/Cours/LesExceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿using LesExceptions.Classes;
+
+double a = 0, b = 0, result = 0;
+
+Console.WriteLine("--------------- LES EXCEPTIONS -----------------");
+
+Console.Write("Veuillez saisir un premier nombre : ");
+
+while (!double.TryParse(Console.ReadLine(), out a))
+    Console.WriteLine("\nErreur de saisie, veuillez entrer un nombre!");
+
+Console.Write("Veuillez saisir un deuxieme nombre : ");
+
+while (!double.TryParse(Console.ReadLine(), out b))
+    Console.WriteLine("\nErreur de saisie, veuillez entrer un nombre!");
+
+try
+{
+    result = ExceptionTest.SafeDivision(a, b);
+    Console.WriteLine($"{a} divisé par {b} est égal à {result}");
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine(e.Message);
+}
+finally
+{
+    Console.WriteLine("Je m'execute quelque soit l'issue du Try/Catch");
+}
+
+Console.WriteLine("Appuyez sur ENTER pour fermer la console...");
+Console.Read();
