cc762b6b2f1957fb9c457fc03dae91485326ce9e
Anthony Di Persio <anthony@utopios.net>
2022-07-22 09:51:18 +0200

    Add IHM Project TpBanqueHeritage

--- FrameworkNET/FormationDotNET/FormationPOO/TpPoo/TpBanqueHeritage/TpBanqueHeritageIHM/Program.cs
+++ FrameworkNET/FormationDotNET/FormationPOO/TpPoo/TpBanqueHeritage/TpBanqueHeritageIHM/Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using TpBanqueHeritageClass.Classes;
-
-Compte c = new();
-Console.WriteLine(c.Id);
-
-
-
-
 Console.WriteLine("Hello, World!");
-Console.Read();
