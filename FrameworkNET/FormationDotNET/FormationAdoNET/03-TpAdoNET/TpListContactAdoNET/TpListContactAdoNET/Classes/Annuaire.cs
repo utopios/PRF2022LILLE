08a39835091f5ed2e09fdec1eb0a8f8e9068a3e7
Anthony Di Persio <anthony@utopios.net>
2022-07-27 17:01:22 +0200

    Add Starter pack

---
+++ FrameworkNET/FormationDotNET/FormationAdoNET/03-TpAdoNET/TpListContactAdoNET/TpListContactAdoNET/Classes/Annuaire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpListContactAdoNET.Classes
+{
+    public class Annuaire
+    {
+        List<Contact> contacts;
+
+        public Annuaire()
+        {
+            Contacts = new();
+        }
+
+        public List<Contact> Contacts { get => contacts; set => contacts = value; }
+    }
+}
