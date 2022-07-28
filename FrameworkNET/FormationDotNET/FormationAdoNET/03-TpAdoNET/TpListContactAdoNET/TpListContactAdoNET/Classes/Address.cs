d48295405e68c5256196f74dbe24a51b1307af85
Anthony Di Persio <anthony@utopios.net>
2022-07-28 12:08:52 +0200

    Update Tp

--- FrameworkNET/FormationDotNET/FormationAdoNET/03-TpAdoNET/TpListContactAdoNET/TpListContactAdoNET/Classes/Address.cs
+++ FrameworkNET/FormationDotNET/FormationAdoNET/03-TpAdoNET/TpListContactAdoNET/TpListContactAdoNET/Classes/Address.cs
@@ -10,7 +10,7 @@ namespace TpListContactAdoNET.Classes
 {
     public class Address
     {
-        private int id;
+        private int addressId;
         private int number;
         private string roadName;
         private int postalCode;
@@ -36,7 +36,7 @@ namespace TpListContactAdoNET.Classes
             Country = country;
         }
 
-        public int Id { get => id; init => id = value; }
+        public int AddressId { get => addressId; init => addressId = value; }
         public int Number { get => number; set => number = value; }
         public string RoadName { get => roadName; set => roadName = value; }
         public int PostalCode { get => postalCode; set => postalCode = value; }
@@ -44,37 +44,98 @@ namespace TpListContactAdoNET.Classes
         public string Country { get => country; set => country = value; }
 
 
-        public Address Get()
+        
+
+        public int Add()
         {
-            string value = "";
             // Connection
             _connection = Connection.New;
+
             // Redaction Request
-            _request = "";
+            _request = "INSERT INTO ADDRESS (Number, Road_Name, Postal_Code, Town, Country) OUTPUT INSERTED.ID " +
+                "VALUES (@Number, @RoadName, @PostalCode, @Town, @Country)";
+
+            // Création objet Command
+            _command = new SqlCommand(_request, _connection);
+
             // Ajout Params
-            _command.Parameters.Add(new SqlParameter("@Alias", value));
+            _command.Parameters.Add(new SqlParameter("@Number", Number));
+            _command.Parameters.Add(new SqlParameter("@RoadName", RoadName));
+            _command.Parameters.Add(new SqlParameter("@PostalCode", PostalCode));
+            _command.Parameters.Add(new SqlParameter("@Town", Town));
+            _command.Parameters.Add(new SqlParameter("@Country", Country));
+
+            // Ouverture de la connection
+            _connection.Open();
+
             // Execution de la commande
+            int addressId = (int)_command.ExecuteScalar();
 
             // Liberations des objets
             _command.Dispose();
             _connection.Close();
 
-            return new Address();
-        }
-
-        public int Add()
-        {
-            return 1;
+            return addressId;
         }
 
         public bool Update()
         {
-            return true;
+            // Connection
+            _connection = Connection.New;
+
+            // Redaction Request
+            _request = "UPDATE ADDRESS SET Number=@Number, Road_Name = @RoadName, Postal_Code = @PostalCode, " +
+                "Town = @Town, Country = @Country WHERE id = @AddressId";
+
+            // Création objet Command
+            _command = new SqlCommand(_request, _connection);
+
+            // Ajout Params
+            _command.Parameters.Add(new SqlParameter("@Number", Number));
+            _command.Parameters.Add(new SqlParameter("@RoadName", RoadName));
+            _command.Parameters.Add(new SqlParameter("@PostalCode", PostalCode));
+            _command.Parameters.Add(new SqlParameter("@Town", Town));
+            _command.Parameters.Add(new SqlParameter("@Country", Country));
+            _command.Parameters.Add(new SqlParameter("@AddressId", AddressId));
+
+            // Ouverture de la connection
+            _connection.Open();
+
+            // Execution de la commande
+            int NbLigne = _command.ExecuteNonQuery();
+
+            // Liberations des objets
+            _command.Dispose();
+            _connection.Close();
+
+            return NbLigne == 1;
         }
 
         public bool Delete()
         {
-            return true;
+            // Connection
+            _connection = Connection.New;
+
+            // Redaction Request
+            _request = "DELETE ADDRESS WHERE Id = @AddressId";
+
+            // Création objet Command
+            _command = new SqlCommand(_request, _connection);
+
+            // Ajout Params
+            _command.Parameters.Add(new SqlParameter("@AddressId", AddressId));
+
+            // Ouverture de la connection
+            _connection.Open();
+
+            // Execution de la commande
+            int NbLigne = _command.ExecuteNonQuery();
+
+            // Liberations des objets
+            _command.Dispose();
+            _connection.Close();
+
+            return NbLigne == 1;
         }
 
         public override string ToString()
